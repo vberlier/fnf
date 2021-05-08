c8fe4f453e3825d2ff0726df271e00239d2a50c7
Valentin Berlier <berlier.v@gmail.com>
2021-05-08 05:54:46 +0200

    Add sprites

---
+++ fnf/sprite.py
@@ -0,0 +1,150 @@
+from dataclasses import dataclass, field
+from typing import Dict, Optional, cast
+from xml.etree import ElementTree
+
+from beet import Context, Model, Texture
+from beet.core.utils import JsonDict, normalize_string
+from PIL import Image
+from pydantic import BaseModel
+
+
+def beet_default(ctx: Context):
+    config = ctx.meta.get("sprite", cast(JsonDict, {}))
+    sprite_manager = ctx.inject(SpriteManager)
+
+    for name, options in config.items():
+        sprite_manager.load(
+            name,
+            texture_url=options["texture"],
+            atlas_url=options.get("atlas"),
+            scale=options.get("scale", 1),
+            display=options.get("display"),
+        )
+
+
+def sprite_override(ctx: Context):
+    sprite_manager = ctx.inject(SpriteManager)
+
+    yield
+
+    ctx.assets["minecraft:item/diamond_hoe"] = Model(
+        {
+            "parent": "minecraft:item/handheld",
+            "textures": {"layer0": "minecraft:item/diamond_hoe"},
+            "overrides": [
+                {"predicate": {"custom_model_data": custom_data}, "model": model_name}
+                for model_name, custom_data in sprite_manager.overrides.items()
+            ],
+        }
+    )
+
+
+class TextureAtlasInfo(BaseModel):
+    x: int
+    y: int
+    width: int
+    height: int
+
+
+@dataclass
+class SpriteManager:
+    ctx: Context
+
+    overrides: Dict[str, int] = field(default_factory=dict)
+
+    def load(
+        self,
+        name: str,
+        texture_url: str,
+        atlas_url: Optional[str] = None,
+        scale: float = 1,
+        display: Optional[JsonDict] = None,
+    ):
+        self.ctx.require(sprite_override)
+
+        cache = self.ctx.cache[__name__]
+        generate = self.ctx.generate["sprite"]
+
+        original = Image.open(cache.download(texture_url))
+
+        size = max(original.size)
+        square = Image.new("RGBA", (size, size), (0, 0, 0, 0))
+        square.paste(original, (0, 0))
+
+        texture_name = generate(name, Texture(square))
+
+        if atlas_url:
+            texture_atlas = ElementTree.parse(cache.download(atlas_url))
+            sprites = {
+                texture_info.attrib["name"]: TextureAtlasInfo(**texture_info.attrib)
+                for texture_info in texture_atlas.getroot()
+            }
+
+            for sprite_name, info in sorted(sprites.items()):
+                model = self.create_sprite(
+                    texture_name,
+                    info.x,
+                    info.y,
+                    info.width,
+                    info.height,
+                    size,
+                    scale,
+                    display,
+                )
+                model_name = generate[name](normalize_string(sprite_name), model)
+                self.overrides[model_name] = len(self.overrides)
+        else:
+            model = self.create_sprite(
+                texture_name,
+                0,
+                0,
+                original.width,
+                original.height,
+                size,
+                scale,
+                display,
+            )
+            model_name = generate(name, model)
+            self.overrides[model_name] = len(self.overrides)
+
+    @staticmethod
+    def create_sprite(
+        texture: str,
+        x: float,
+        y: float,
+        width: float,
+        height: float,
+        size: float,
+        scale: float,
+        display: Optional[JsonDict] = None,
+    ) -> Model:
+        factor = 16 / size
+        uv = x * factor, y * factor, (x + width) * factor, (y + height) * factor
+
+        width *= scale
+        height *= scale
+
+        left = factor * (size - width) / 2
+        top = factor * (size - height) / 2
+        right = factor * (left + width)
+        bottom = factor * (top + height)
+
+        model = Model(
+            {
+                "textures": {"all": texture},
+                "elements": [
+                    {
+                        "from": [left, top, 8],
+                        "to": [right, bottom, 8],
+                        "faces": {
+                            "north": {"uv": uv, "texture": "#all"},
+                        },
+                    }
+                ],
+            }
+        )
+
+        if display:
+            model.data["display"] = {"head": display}
+
+        return model
